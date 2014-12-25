202b62d10f9d06e3f8f7cf322bf615e6046bd633
LinhPhi <linhphi9x94@gmail.com>
2014-12-25 13:57:34 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_bacon-user
-add_lunch_combo cm_bacon-userdebug
-add_lunch_combo cm_bacon-eng
+add_lunch_combo greenos_bacon-user
+add_lunch_combo greenos_bacon-userdebug
+add_lunch_combo greenos_bacon-eng
